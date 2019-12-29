9213fe010ad0bb8c0eaca1ac5ff60cb214d1ea69
tom_glb <goodMorning_glb@atguigu.com>
2019-12-29 13:52:40 +0800

    append firstName attribute for Person class

--- src/com/itheima/test/Person.java
+++ src/com/itheima/test/Person.java
@@ -15,6 +15,16 @@ public class Person {
 	private Integer age;
 	
 	private String lastName;
+	
+	private String firstName;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 
 	public String getLastName() {
 		return lastName;
